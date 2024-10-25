d3a90ff94daba85e76f3633a7a467f2dabd06c3f
SaraySantiago <ssantiago18@iesrioarba.es>
2024-10-25 13:32:16 +0200

    .

--- Procedimiento.puro.py
+++ Procedimiento.puro.py
@@ -1,19 +1,27 @@
 #C:\Users\Admin\AppData\Local\Microsoft\WindowsApps\python.exe
 
-__objeto = 3  #objeto y estado global
+__objeto = [11, 22, 33]  #objeto y estado global
             #lo alcanza todo el codigo
 
 
-def doblar (): 
-    """procedimiento puro que doblar"""
+def doblarLista (posicion): 
+    """procedimiento puro que doblar un item de una lista particular __objeto, que es global"""
     global __objeto 
-    __objeto = __objeto * 2
-
+    __objeto[posicion] = __objeto[posicion] * 2
+    
+def doblar (lista, donde):
+    pass  
+# TO DO: que funcione!
 
 if __name__ == "__main__":
     
-    print (__objeto) # 3
-    doblar () # procedimiento puro
-    print(__objeto) # 6
+    print (__objeto) # [11,22,33]
+    doblarLista (1) # procedimiento puro
+    print(__objeto) # [11,44,33]
  
+    lista0= ["ma", "pa"]
+ 
+    print (lista0) # [ma, pa]
+    doblar (lista0, 1) # procedimiento puro
+    print(lista0) # [ma, papa]
     
